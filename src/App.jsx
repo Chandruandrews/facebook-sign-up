bc4c7e3f7e6411c93d170b8d66c0a9925f40140d
chandru A <chandruandrews69@gmail.com>
2025-03-27 11:09:52 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,80 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+const dates=[];
+const months=["Jan","Feb","mar","Apr","May","June","July","Aus","sep","Oct","Nov","Dec"]
+const years=[];
+const genders=["Male","Female","Others"];
+
+const [firstname,setfirstname]= useState("");
+const [lastname,setlastname]= useState("");
+const [date,setdate]= useState("");
+const [mont,setmonth]= useState("");
+const [yearr,setyear]= useState("");
+const [gen,setgender]= useState("");
+const [email,setemail]= useState("");
+const [password,setpassword]= useState("");
+
+for(let i=1;i<=30;i++){
+    dates.push(i);
+}
+for(let i=1985;i<=2025;i++){
+    years.push(i);
+}
+
+const handlefirstname=(event)=>{
+    setfirstname(event.target.value);
+    console.log("firstname:",event.target.value)
+}
+
+const handlelastname=(event)=>{
+    setlastname(event.target.value);
+    console.log("lastname:",event.target.value)
+}
+
+const handledate=(event)=>{
+    setdate(event.target.value);
+    console.log("date",event.target.value)
+}
+
+const handlemont=(event)=>{
+    setmonth(event.target.value);
+    console.log("month:",event.target.value)
+}
+
+const handleyear=(event)=>{
+    setyear(event.target.value);
+    console.log("yearr:",event.target.value)
+}
+
+const handlegender=(event)=>{
+    setgender(event.target.value);
+    console.log("setgender:",event.target.value);
+}
+const handleemail=(event)=>{
+    setemail(event.target.value);
+    console.log("Email:",event.target.value);
+}
+
+const handlepassword=(event)=>{
+    setpassword(event.target.value);
+    console.log("password:",event.target.value);
+}
+
+const signup=()=>{
+    const userdata={
+        firstname,
+        lastname,
+        date,
+        mont,
+        yearr,
+        gen,
+        email,
+        password,
+    };
+    console.log("userdata:",userdata)
+    alert("Signup Successfull");
+}
   return (
     <div class="signup-wrapper">
     <h1 class="logo-text">facebook</h1>
@@ -11,107 +85,57 @@ function App() {
         <hr />
 
         <div class="name-fields">
-            <input type="text" placeholder="First name" />
-            <input type="text" placeholder="Last name" />
+            <input type="text" placeholder="First name" value={firstname} onChange={handlefirstname} />
+            <input type="text" placeholder="Last name" value={lastname} onChange={handlelastname}/>
         </div>
 
         <label class="form-label">Date of birth</label>
         <div class="date-fields">
-            <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>6</option>
-                <option>7</option>
-                <option>8</option>
-                <option>9</option>
-                <option>10</option>
-                <option>11</option>
-                <option>12</option>
-                <option>13</option>
-                <option>14</option>
-                <option>15</option>
-                <option>16</option>
-                <option>17</option>
-                <option>18</option>
-                <option>19</option>
-                <option>20</option>
-                <option>21</option>
-                <option>22</option>
-                <option>23</option>
-                <option>24</option>
-                <option>25</option>
-                <option>26</option>
-                <option>27</option>
-                <option>28</option>
-                <option>29</option>
-                <option>30</option>
+            <select id="" value={date} onChange={handledate} >
+                {
+                    dates.map(day=>(
+                        <option value={day}>{day}</option>
+                    ))
+                }
  
             </select>
-            <select id="">
-                <option>Jan</option>
-                <option>Feb</option>
-                <option>Mar</option>
-                <option>Apral</option>
-                <option>May</option>
-                <option>June</option>
-                <option>July</option>
-                <option>Aagust</option>
-                <option>Sep</option>
-                <option>Oct</option>
-                <option>Nav</option>
-                <option>Dec</option>
-                
+            <select id="" value={mont} onChange={handlemont} >
+            {
+                    months.map(month =>(
+                        <option value={month}>{month}</option>
+                    ))
+            }
             </select>
           
-            <select id="">
-                <option>2001</option>
-                <option>2002</option>
-                <option>2003</option>
-                <option>2004</option>
-                <option>2005</option>
-                <option>2006</option>
-                <option>2007</option>
-                <option>2008</option>
-                <option>2009</option>
-                <option>2010</option>
-                <option>2011</option>
-                <option>2012</option>
-                <option>2013</option>
-                <option>2014</option>
-                <option>2015</option>
-                <option>2016</option>
-                <option>2017</option>
-                <option>2018</option>
-                <option>2019</option>
-                <option>2020</option>
-                <option>2021</option>
-                <option>2022</option>
-                <option>2023</option>
-                <option>2024</option>
-                <option>2025</option>
-                
+            <select id="" value={yearr} onChange={handleyear}>
+            {
+                    years.map(year =>(
+                        <option value={year} >{year}</option>
+                    ))
+            }
             </select>
 
         </div>
 
         <label class="form-label">Gender</label>
-        <div class="gender-selection">
-            <label><input type="radio" name="gender" /> Female</label>
-            <label><input type="radio" name="gender" /> Male</label>
-            <label><input type="radio" name="gender" /> Custom</label>
+        <div class="gender-selection" value={gen} onChange={handlegender} >
+            {   
+                
+                genders.map(gender=>(
+                    <label><input value={gender} type="radio" name="gender" />{gender}</label>
+                ))
+            }
+            
         </div>
 
-        <input type="email" placeholder="Email or phone number" />
-        <input type="password" placeholder="New password" />
+        <input type="email" placeholder="Email or phone number" value={email}onChange={handleemail} />
+        <input type="password" placeholder="New password" value={password}onChange={handlepassword}/>
 
         <p class="terms-text">
             By clicking Sign Up, you agree to our <a href="#">Terms</a> and <a href="#">Privacy Policy</a>.
         </p>
 
-        <button class="signup-button">Sign Up</button>
+        <button class="signup-button" onClick={signup}>Sign Up</button>
         <p class="login-text">Already have an account? <a href="#">Log in</a></p>
     </div>
 </div>
